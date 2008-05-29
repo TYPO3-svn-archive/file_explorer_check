fd1c284f498c0110754d2f2c9e06ad74752b6335
phlogiston <phlogiston@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-05-29 13:20:02 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/file_explorer_check/trunk@9753 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "file_explorer_check"
 #
-# Auto generated 22-05-2008 15:48
+# Auto generated 29-05-2008 15:15
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.2',
+	'version' => '0.1.3',
 	'constraints' => array(
 		'depends' => array(
 			'file_explorer' => '2.0.0',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"9630";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"749a";s:14:"ext_tables.php";s:4:"4b0d";s:14:"doc/manual.sxw";s:4:"4ce7";s:19:"doc/wizard_form.dat";s:4:"5aa3";s:20:"doc/wizard_form.html";s:4:"6441";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"a822";s:14:"mod1/index.php";s:4:"70f9";s:18:"mod1/locallang.xml";s:4:"85e2";s:22:"mod1/locallang_mod.xml";s:4:"91a6";s:19:"mod1/moduleicon.gif";s:4:"749a";}',
+	'_md5_values_when_last_written' => 'a:13:{s:9:"ChangeLog";s:4:"ff43";s:10:"README.txt";s:4:"3c56";s:12:"ext_icon.gif";s:4:"749a";s:14:"ext_tables.php";s:4:"4b0d";s:14:"doc/manual.sxw";s:4:"a2fb";s:19:"doc/wizard_form.dat";s:4:"5aa3";s:20:"doc/wizard_form.html";s:4:"6441";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"05d6";s:14:"mod1/index.php";s:4:"993d";s:18:"mod1/locallang.xml";s:4:"85e2";s:22:"mod1/locallang_mod.xml";s:4:"91a6";s:19:"mod1/moduleicon.gif";s:4:"749a";}',
 	'suggests' => array(
 	),
 );
